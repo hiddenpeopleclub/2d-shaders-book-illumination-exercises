bd35ae48b0da54e51c26e268dd0d7ddea1d5cd67
Francisco Tufro <info@franciscotufro.com>
2018-07-23 11:17:43 -0300

    Added Exercise 1

---
+++ Exercise 1 - Static Lights and Shadows/Assets/Scripts/MovingCharacter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingCharacter : MonoBehaviour {
+
+    public Vector3 StartPosition;
+    public Vector3 EndPosition;
+    public float Duration = 2;
+    float Timer = 0;
+
+    void Update()
+    {
+        Timer += Time.deltaTime;
+        transform.position = Vector3.Lerp(StartPosition, EndPosition, Timer / Duration);
+
+        if (Timer >= Duration)
+            Timer = 0;
+    }
+}
