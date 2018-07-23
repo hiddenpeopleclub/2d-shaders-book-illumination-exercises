6bc87d250ecc0d1b64216c9e843cb64338c3a33d
Francisco Tufro <info@franciscotufro.com>
2018-07-23 13:34:50 -0300

    Added exercise 2.

---
+++ Exercise 2 - Ambient Lights/Assets/Scripts/SpriteLight.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteLight : MonoBehaviour 
+{
+    public enum LightTypes
+    {
+        Ambient,
+        Directional,
+        Point
+    };
+
+    // Light properties (Colors, Direction, LightType, etc)
+    // ...
+}
---
+++ Exercise 2 - Ambient Lights/Assets/Scripts/SpriteWithLights.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpriteWithLights : MonoBehaviour {
+
+	void Start () 
+    {
+	    	
+	}
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        SpriteLight spriteLight = collision.gameObject.GetComponent<SpriteLight>();
+
+        if (spriteLight)
+        {
+            // Check Light type and set the character up accordingly.
+        }
+
+    }
+
+
+}
